58b2f0cfe80ebb584cb994d275924dda352ac291
SeanDu <aijisud@gmail.com>
2018-03-06 23:41:33 +0800

    cwd

--- etfshares/getszseetf.py
+++ etfshares/getszseetf.py
@@ -5,13 +5,15 @@ from urllib.request import urlretrieve
 import os
 import time
 
+
+cwd = os.getcwd()
 today = time.strftime("%Y%m%d")
 
-URL = "http://www.szse.cn/szseWeb/ShowReport.szse?SHOWTYPE=xlsx&CATALOGID=1945&ENCODE=1&TABKEY=tab1"
 DIR = "data"
-sse_file = today + "_sse_" + "ETF列表.xlsx"
+URL = "http://www.szse.cn/szseWeb/ShowReport.szse?SHOWTYPE=xlsx&CATALOGID=1945&ENCODE=1&TABKEY=tab1"
+sse_file = today + "_szse_" + "ETF列表.xlsx"
 
-file_name = os.path.join(DIR, sse_file)
+file_name = os.path.join(cwd, DIR, sse_file)
 
 urlretrieve(URL, file_name)
 
